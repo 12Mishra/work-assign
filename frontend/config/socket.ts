ee34aaf8248b02979fa4762f3b3fe95dd467fadf
Abhyuday Mishra <88658148+12Mishra@users.noreply.github.com>
2025-07-18 17:52:18 +0530

    socket-file-corrected

--- frontend/config/socket.ts
+++ frontend/config/socket.ts
@@ -3,6 +3,8 @@ import { io } from "socket.io-client";
 const socket = io(`${process.env.NEXT_PUBLIC_BACKEND_URL}`, {
   withCredentials: true,
   transports: ["websocket"],
-}); 
+  secure: true,
+  reconnection: true,
+});
 
 export default socket;
